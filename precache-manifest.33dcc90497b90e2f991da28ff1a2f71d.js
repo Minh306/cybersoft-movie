cf3cf8dde7e5e9ffa77083e17aab733f2549c66a
Le Quang Minh <minh.hii306@gmail.com>
2021-11-10 11:13:35 -0800

    Updates

---
+++ precache-manifest.33dcc90497b90e2f991da28ff1a2f71d.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848810f6fe807d7e546e4ff84fec91ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e25f1b626912afef078c",
+    "url": "/static/css/2.642d608e.chunk.css"
+  },
+  {
+    "revision": "ad3c870d812b42b25214",
+    "url": "/static/css/main.4e712336.chunk.css"
+  },
+  {
+    "revision": "e25f1b626912afef078c",
+    "url": "/static/js/2.6f41f835.chunk.js"
+  },
+  {
+    "revision": "4fa91b7b559be0d9ba2565f00bcc2f4b",
+    "url": "/static/js/2.6f41f835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d5930a1dec7b2af851",
+    "url": "/static/js/3.bd7082a8.chunk.js"
+  },
+  {
+    "revision": "ad3c870d812b42b25214",
+    "url": "/static/js/main.bc93ce5e.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.bc93ce5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbb50db3e7fa8eba346",
+    "url": "/static/js/runtime-main.09378bde.js"
+  },
+  {
+    "revision": "2bf226f2d66c0b4f9894fd1ef0da300f",
+    "url": "/static/media/app-slide-1.2bf226f2.jpg"
+  },
+  {
+    "revision": "d3837b8bfe88dd811a8cd1e662c66d65",
+    "url": "/static/media/app-slide-2.d3837b8b.jpg"
+  },
+  {
+    "revision": "14dc0a92fb1df01349fb28c1d69c3c13",
+    "url": "/static/media/app-slide-3.14dc0a92.jpg"
+  },
+  {
+    "revision": "84292cf4137d9709f7466d5dc883f4d5",
+    "url": "/static/media/app-slide-4.84292cf4.jpg"
+  },
+  {
+    "revision": "049c6333ccc1701f4882aa209bfdee7c",
+    "url": "/static/media/app-slide-5.049c6333.jpg"
+  },
+  {
+    "revision": "004872c7fd28cb61c405bc2784782dde",
+    "url": "/static/media/app-slide-6.004872c7.jpg"
+  },
+  {
+    "revision": "3c251b277272c59317c2c58fe6106cc1",
+    "url": "/static/media/app-slide-7.3c251b27.jpg"
+  },
+  {
+    "revision": "a0eea0a08ea2dd56af5bceeb0291e3dc",
+    "url": "/static/media/app-slide-8.a0eea0a0.jpg"
+  },
+  {
+    "revision": "5ce22c7dd7d9fecf8a5aa17dc98ab701",
+    "url": "/static/media/app-slide-9.5ce22c7d.jpg"
+  },
+  {
+    "revision": "d713fc3a3e00188abf478fc9ee7a9690",
+    "url": "/static/media/back-news.d713fc3a.png"
+  },
+  {
+    "revision": "5de6f656b882b385e291b50b8ddb7372",
+    "url": "/static/media/background-app.5de6f656.jpg"
+  },
+  {
+    "revision": "026eeed95912db4e863c2ab7e376567d",
+    "url": "/static/media/banner1.026eeed9.jpg"
+  },
+  {
+    "revision": "200baf009a791a83afcfc3a582712b19",
+    "url": "/static/media/banner2.200baf00.png"
+  },
+  {
+    "revision": "273aedb1628d50b8984ffe3a207e27a3",
+    "url": "/static/media/banner3.273aedb1.jpg"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/static/media/bg-signupin.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "d7c743aee9585a8be2455df5fa8638bb",
+    "url": "/static/media/contact-icon-12.d7c743ae.png"
+  },
+  {
+    "revision": "184067dcffebc899e4591756ee08fa58",
+    "url": "/static/media/contact-icon-5.184067dc.png"
+  },
+  {
+    "revision": "440b9b231a84584c31de471bd31d0985",
+    "url": "/static/media/default-image.440b9b23.jpg"
+  },
+  {
+    "revision": "f3d3b02c52d6144889ea6a5cbc6f0f19",
+    "url": "/static/media/footer-thongbao-img.f3d3b02c.png"
+  },
+  {
+    "revision": "1690d974372a2078683943a075e66d44",
+    "url": "/static/media/khai-truong-rap-xin-gia-ngon-chuan-xi-tai-sai-gon-16111317082644.1690d974.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "a3e936408e4eb108224f1050cab94797",
+    "url": "/static/media/new10.a3e93640.png"
+  },
+  {
+    "revision": "255e186d58e39fa689bba30e8582c6a0",
+    "url": "/static/media/new11.255e186d.png"
+  },
+  {
+    "revision": "8d36f38947acb1d4da3d07545d6f6c4e",
+    "url": "/static/media/new12.8d36f389.png"
+  },
+  {
+    "revision": "86be4db0cc10a07d69456412ee40fe3a",
+    "url": "/static/media/new13.86be4db0.png"
+  },
+  {
+    "revision": "b8dd181f57ab5c31ecd81f5e2f26f0e8",
+    "url": "/static/media/new14.b8dd181f.jpg"
+  },
+  {
+    "revision": "957071b935e30c65e81384d90556e4df",
+    "url": "/static/media/new15.957071b9.jpg"
+  },
+  {
+    "revision": "fa660ee1b2f983a38970dd047c0186e0",
+    "url": "/static/media/new16.fa660ee1.jpg"
+  },
+  {
+    "revision": "65ca90de9371de887b32ccb64a848a5b",
+    "url": "/static/media/new17.65ca90de.jpg"
+  },
+  {
+    "revision": "fec0de86a890c2f8005959d61a96c1c9",
+    "url": "/static/media/new18.fec0de86.jpg"
+  },
+  {
+    "revision": "2601fae5d0bc20bbd0f3f15723b6afa0",
+    "url": "/static/media/new19.2601fae5.png"
+  },
+  {
+    "revision": "b5453b56e246bf9faa168b95618784ba",
+    "url": "/static/media/new2.b5453b56.jpg"
+  },
+  {
+    "revision": "3e593b54336596a30af5e898db452a6b",
+    "url": "/static/media/new20.3e593b54.jpg"
+  },
+  {
+    "revision": "4844247280044d4ffd2c05c66749bf8b",
+    "url": "/static/media/new21.48442472.png"
+  },
+  {
+    "revision": "ca32297cf707b399bc806c5a57600db4",
+    "url": "/static/media/new22.ca32297c.jpg"
+  },
+  {
+    "revision": "0e1b5b945449a3af9af7386d35f21a02",
+    "url": "/static/media/new23.0e1b5b94.jpg"
+  },
+  {
+    "revision": "50a3cc8a5fbbdba4175f73dafab62964",
+    "url": "/static/media/new24.50a3cc8a.jpg"
+  },
+  {
+    "revision": "4be90c53ae4e23a74972a7b074fab9a3",
+    "url": "/static/media/new4.4be90c53.jpg"
+  },
+  {
+    "revision": "0c59a5b46e30cd2cb9427a180cd9132e",
+    "url": "/static/media/new5.0c59a5b4.jpg"
+  },
+  {
+    "revision": "dce8f791274cc0640cb6232600c494e3",
+    "url": "/static/media/new6.dce8f791.png"
+  },
+  {
+    "revision": "fb7915b4424cd1eda6b6ea363187520a",
+    "url": "/static/media/new9.fb7915b4.png"
+  },
+  {
+    "revision": "f314cfc7df9e5640915f650654a14458",
+    "url": "/static/media/news-1.f314cfc7.png"
+  },
+  {
+    "revision": "e53d04a4498ee10a78ea7f6aa571702b",
+    "url": "/static/media/news-small-1.e53d04a4.png"
+  },
+  {
+    "revision": "dccaca1e82c43ad5b8a4c3d65c69b3ad",
+    "url": "/static/media/news-small-2.dccaca1e.png"
+  },
+  {
+    "revision": "317fab653073129c2820868c2de1a8d3",
+    "url": "/static/media/payment-zalo.317fab65.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
